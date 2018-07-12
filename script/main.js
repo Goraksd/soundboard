dfcb0af40d21c1922b2b60c068a226ca70ae12f7
Goraksd <gskakunov518@gmail.com>
2018-07-12 10:51:54 +0300

    Finally

--- script/main.js
+++ script/main.js
@@ -16,11 +16,43 @@
     key.classList.remove('playing');
   }
 
-  elem.onclick = function() {
-    function playSound(m)
-    
-  };
+  function mouseDown1() {
+    var audio = new Audio('sound/synthesizer02.wav');
+    audio.play();
+    }
 
-  window.addEventListener('onclick', playSound)
+  function mouseDown2() {
+    var audio = new Audio('sound/drum_bass.wav');
+    audio.play();
+    }
+  function mouseDown3() {
+    var audio = new Audio('sound/piano1.wav');
+    audio.play();
+    }
+  function mouseDown4() {
+    var audio = new Audio('sound/piano3.wav');
+    audio.play();
+    }
+  function mouseDown5() {
+    var audio = new Audio('sound/piano5.wav');
+    audio.play();
+    }
+  function mouseDown6() {
+    var audio = new Audio('sound/synthesizer01.wav');
+    audio.play();
+    }
+  function mouseDown7() {
+    var audio = new Audio('sound/synthesizer06.wav');
+    audio.play();
+    }
+  function mouseDown8() {
+    var audio = new Audio('sound/synthesizer13.wav');
+    audio.play();
+    }
+  function mouseDown9() {
+    var audio = new Audio('sound/synthesizer23.wav');
+    audio.play();
+    }
   window.addEventListener('keydown', playSound)
   window.addEventListener('keyup', endSound) 
+  window.addEventListener('onclick', playSound)
